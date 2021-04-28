f305d244ac111297e7480a0358b28f2aaba03cee
GregorioFornetti <fornetti.greg@gmail.com>
2021-04-28 17:54:17 -0300

    .
    - Novos sprites de esteiras aplicados
    - Novo menu principal aplicado
    - Animação do menu principal
    - Colisao de camera e zoom arrumados para as fases 16-20
    - Poças de sangue colocadas nas fases de 16-20

--- menus/Menu_principal.gd
+++ menus/Menu_principal.gd
@@ -1,8 +1,14 @@
 extends Control
 
+onready var animation_player = $AnimationPlayer
+onready var timer = $Timer_animacao
 
 func _on_Btn_jogar_pressed():
 	get_tree().change_scene("res://menus/Menu_saves.tscn")
 
 func _on_Btn_sair_pressed():
 	get_tree().quit()
+
+func _on_Timer_animacao_timeout():
+	animation_player.play("BackgroundAnim")
+	timer.start(animation_player.current_animation_length + rand_range(5, 35))
