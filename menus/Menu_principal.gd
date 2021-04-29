17a46769a063cccea8159151c0a477da887247d8
GregorioFornetti <fornetti.greg@gmail.com>
2021-04-29 12:18:36 -0300

    mudança no menu principal
    animação melhorada

--- menus/Menu_principal.gd
+++ menus/Menu_principal.gd
@@ -2,6 +2,12 @@ extends Control
 
 onready var animation_player = $AnimationPlayer
 onready var timer = $Timer_animacao
+onready var background = $Background
+const FRAME_BACKGROUND_PLAYER = 0
+const FRAME_BACKGROUND_CONSCIENCIA = 3
+
+func _ready():
+	randomize()
 
 func _on_Btn_jogar_pressed():
 	get_tree().change_scene("res://menus/Menu_saves.tscn")
@@ -10,5 +16,14 @@ func _on_Btn_sair_pressed():
 	get_tree().quit()
 
 func _on_Timer_animacao_timeout():
-	animation_player.play("BackgroundAnim")
-	timer.start(animation_player.current_animation_length + rand_range(5, 35))
+	if background.frame == FRAME_BACKGROUND_PLAYER:
+		if rand_range(1, 100) <= 75:
+			animation_player.play("TransicaoParaPlayer")
+			timer.start(animation_player.current_animation_length + rand_range(5, 35))
+		else:
+			animation_player.play("TransicaoParaConsciencia")
+			timer.start(animation_player.current_animation_length + rand_range(2, 6))
+	else:
+		animation_player.play("TransicaoParaPlayer")
+		timer.start(animation_player.current_animation_length + rand_range(5, 35))
+	
